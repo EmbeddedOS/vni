84b47834e226a2a2490a73b66a75b367190b3643
LaVa264 <nguyenthanhcong12gtm1@gmail.com>
2024-05-24 07:42:53 -0700

    Implement transmitter raw layer 2 packet.

--- transmitter.c
+++ transmitter.c
@@ -3,30 +3,56 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <linux/if_packet.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
 
+#define INTERFACE_MAX_LENGTH 20
+#define DATA    "Hi There"
+
 int main(int argc, char **argv)
 {
+    char interface[INTERFACE_MAX_LENGTH] = {0};
     int res = 0;
-    int fd = socket(AF_PACKET, SOCK_RAW, 0);
+    int fd = 0;
+    struct ifreq interface_idx = {0};
+    struct ifreq interface_addr = {0};
+    struct sockaddr_ll socket_address = {0};
+
+    if (argc < 2)
+    {
+        printf("Please specify interface name!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    strncpy(interface, argv[1], INTERFACE_MAX_LENGTH);
+
+    /* 1. Create raw socket layer 2. */
+    fd = socket(AF_PACKET, SOCK_RAW, 0);
     if (fd < 0)
     {
         printf("Cannot create socket: -%d\n", errno);
         exit(EXIT_FAILURE);
     }
 
-    res = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, "vni0", strlen("vni0") + 1);
-    if (res < 0)
+    /* 2. Get the interface index. */
+    strncpy(interface_idx.ifr_name, interface, INTERFACE_MAX_LENGTH);
+    if (ioctl(fd, SIOCGIFINDEX, &interface_idx) < 0)
     {
-        printf("Cannot setsockopt: -%d\n", errno);
+        printf("Cannot get interface index: -%d\n", errno);
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    res = send(fd, "Hi there", strlen("Hi There"), 0);
+    socket_address.sll_ifindex = interface_idx.ifr_ifindex;
+
+    /* 3. Send data to the index. */
+    res = sendto(fd, DATA, strlen(DATA), 0,
+                 (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll));
     if (res < 0)
     {
         printf("Cannot send: -%d\n", errno);
